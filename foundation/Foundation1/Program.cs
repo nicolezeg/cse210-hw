37e79e4c9da2b976ef58692b2ca1be9a39dbb5cc
nicolezeg <nzegarra07@gmail.com>
2024-10-13 02:12:27 -0500

    Foundation 1

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         var video1 = new Video("How to make a website using HTML and CSS?", "Alice", 300);
         video1.AddComment(new Comment("Diego", "This is a great video!"));
-        video1.AddComment(new Comment("Rosa", "I agree!"));
+        video1.AddComment(new Comment("Rosa", "Awesome content!"));
         video1.AddComment(new Comment("Spencer", "Not bad."));
 
         var video2 = new Video("Understanding C#", "Eve", 400);
@@ -16,7 +16,7 @@ class Program
 
         var video3 = new Video("Mastering HTML", "Christian", 500);
         video3.AddComment(new Comment("Jake", "This video is amazing!"));
-        video3.AddComment(new Comment("Adriel", "I know, right?"));
+        video3.AddComment(new Comment("Adriel", "I learned a lot."));
         video3.AddComment(new Comment("Nicolas", "Best video ever!"));
 
         var videos = new List<Video> { video1, video2, video3 };
