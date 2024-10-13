e05a6866948d63979f156aea325e4eb7eb5fac43
nicolezeg <nzegarra07@gmail.com>
2024-10-13 02:08:59 -0500

    Foundation 1

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -4,20 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        var video1 = new Video("Video 1", "Author 1", 300);
-        video1.AddComment(new Comment("Commenter 1", "This is a great video!"));
-        video1.AddComment(new Comment("Commenter 2", "I agree!"));
-        video1.AddComment(new Comment("Commenter 3", "Not bad."));
+        var video1 = new Video("How to make a website using HTML and CSS?", "Alice", 300);
+        video1.AddComment(new Comment("Diego", "This is a great video!"));
+        video1.AddComment(new Comment("Rosa", "I agree!"));
+        video1.AddComment(new Comment("Spencer", "Not bad."));
 
-        var video2 = new Video("Video 2", "Author 2", 400);
-        video2.AddComment(new Comment("Commenter 4", "This video is okay."));
-        video2.AddComment(new Comment("Commenter 5", "I like it!"));
-        video2.AddComment(new Comment("Commenter 6", "Not my favorite."));
+        var video2 = new Video("Understanding C#", "Eve", 400);
+        video2.AddComment(new Comment("Sarah", "This video is okay."));
+        video2.AddComment(new Comment("Nicole", "I like it!"));
+        video2.AddComment(new Comment("Jonathan", "Not my favorite."));
 
-        var video3 = new Video("Video 3", "Author 3", 500);
-        video3.AddComment(new Comment("Commenter 7", "This video is amazing!"));
-        video3.AddComment(new Comment("Commenter 8", "I know, right?"));
-        video3.AddComment(new Comment("Commenter 9", "Best video ever!"));
+        var video3 = new Video("Mastering HTML", "Christian", 500);
+        video3.AddComment(new Comment("Jake", "This video is amazing!"));
+        video3.AddComment(new Comment("Adriel", "I know, right?"));
+        video3.AddComment(new Comment("Nicolas", "Best video ever!"));
 
         var videos = new List<Video> { video1, video2, video3 };
 
