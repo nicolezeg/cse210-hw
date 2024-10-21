4a1b6003847c81d2b9ed77d2833543d12d36d752
nicolezeg <nzegarra07@gmail.com>
2024-10-21 11:50:08 -0500

    Develop05

--- prove/Develop05/BreathingActivity.cs
+++ prove/Develop05/BreathingActivity.cs
@@ -1,8 +1,7 @@
 using System;
-using System.Collections.Generic;
 
 public class BreathingActivity : Activity
-{
+{  
     public BreathingActivity() : base("Breathing", "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.", 50)
     {
     }
@@ -11,13 +10,11 @@ public class BreathingActivity : Activity
     {
 
         DisplayStartingMessage();
-        int _duration = int.Parse(Console.ReadLine());
 
-        for (int i = 3; i > _duration; i--)
+        for (int i = 0; i > 3; i++)
         {
             Console.WriteLine("Breathe in...");
             ShowCountdown(3);
-            Thread.Sleep(1000);
             Console.WriteLine("Breathe out...");
             ShowCountdown(3);
         }
